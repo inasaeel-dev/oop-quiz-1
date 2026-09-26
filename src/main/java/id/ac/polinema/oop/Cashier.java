4955e56230e9a7faf10d3e6bdc228bb18cd3c6c0
Inas Asami El <inasasami11@gmail.com>
2026-09-26 08:55:27 +0700

    kuis pbo

--- src/main/java/id/ac/polinema/oop/Cashier.java
+++ src/main/java/id/ac/polinema/oop/Cashier.java
@@ -3,7 +3,7 @@ package id.ac.polinema.oop;
 public class Cashier {
   public double calculateChange(Order order, double cash){
     if (order != null){
-      double total = order.calculateTotal();
+      double total = order.getTotal();
       return cash - total;
     }
     return cash;
